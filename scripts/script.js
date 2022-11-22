232df64297c4ae2d8a5e70a1500ec1e266e6246f
Saad Al-Mridha <salmridha44@gmail.com>
2022-11-22 14:56:32 -0800

    added functions to the button where it only clicks once

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,31 @@
+// const clickButton = document.querySelector('.click-me');
+//     // console.log(clickButton);
+//     function clickHandler()
+//     {
+//         alert('You clicked me!')
+//     }
+//     clickButton.addEventListener('click', clickHandler);
+// // above is a function that alerts user a message when button is clicked
+
+
+
+// const clickButton = document.querySelector('.click-me');
+//     // console.log(clickButton);
+//     function clickHandler()
+//     {
+//         alert('You clicked me!')
+//         clickButton.removeEventListener('click', clickHandler);
+//     }
+//     clickButton.addEventListener('click', clickHandler);
+// // above only clicks one time because of removeEventListener inside the function.
+
+
 const clickButton = document.querySelector('.click-me');
     // console.log(clickButton);
     function clickHandler()
     {
         alert('You clicked me!')
     }
-    clickButton.addEventListener('click', clickHandler);
+    clickButton.addEventListener('click', clickHandler, {once:true});
+// above does the same thing as the second one but uses a boolean. 
+
